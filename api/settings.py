f3d63856c6c5a6ab7c2ae8642011c6a6309ccd73
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-24 12:10:01 +0300

    added new tests to ensure one can post to the database

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3@xwr_gl-1aro6x)^6n_069=l6+9so8_88s8o(j%zv**3vs^0v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
--- restframework/tests.py
+++ restframework/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
-
+from .models import Bucketlist
 # Create your tests here.
+
+class ModelTestCase(TestCase):
+    '''
+    Class that defines the test suite for the bucketlist models
+    '''
+    def setUp(self):
+        '''
+        define the test client and other test variables
+        '''
+        self.bucketlist_name = "Write world class code "
+        self.bucketlist = Bucketlist(name = self.bucketlist_name)
+
+    def test_create_model(self):
+        '''
+        Test the bucketlist model can create a bucketlist
+        '''
+        old_count = Bucketlist.objects.count()
+        self.bucketlist.save()
+        new_count = Bucketlist.objects.count()
+        self.assertNotEqual(old_count, new_count)
