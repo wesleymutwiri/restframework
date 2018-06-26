3a295ad31216ca6ea8bbb5d5a77c535b1de9339a
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-26 14:34:07 +0300

    added tests to the application

--- restframework/tests.py
+++ restframework/tests.py
@@ -43,3 +43,20 @@ class ViewTestCase(TestCase):
         Test the api has capability to create data
         '''
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_api_can_get_bucketlist(self):
+        '''
+        Test the api can get a given bucketlist
+        '''
+        bucketlist = Bucketlist.objects.get()
+        response = self.client.get(
+            reverse('details', kwargs={'pk': bucketlist.id}),format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, bucketlist)
+
+    def test_api_can_update_bucketlist(self):
+        '''
+        Test if the api can update a given bucketlist
+        '''
+        change_bucketlist = {'name':'Something new'}
